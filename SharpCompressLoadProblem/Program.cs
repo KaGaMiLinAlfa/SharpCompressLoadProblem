7ab0d86f2c290393f1df39b265291d6bd6b902a9
KaGaMi <kagamilin10271@gmail.com>
2023-09-15 11:55:18 +0800

    添加项目文件。

---
+++ SharpCompressLoadProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace SharpCompressLoadProblem
+{
+    internal class Program
+    {
+        static string subroutineUrl = @"C:\Users\KaGaMi\source\repos\SharpCompressLoadProblem\Subroutine\bin\Release\netcoreapp3.1\publish";
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+            var assembly = Assembly.LoadFrom(Path.Combine(subroutineUrl, "Subroutine.dll"));
+            var type = assembly.GetType("Subroutine.Class1");
+            object obj = Activator.CreateInstance(type);
+            MethodInfo method = type.GetMethod("Run");
+            method.Invoke(obj, null);
+        }
+
+        private static Assembly ResolveAssembly(object sender, ResolveEventArgs e)
+        {
+            string dllName = e.Name.Split(new[] { ',' })[0] + ".dll";
+            return Assembly.LoadFrom(Path.Combine(subroutineUrl, dllName));
+        }
+    }
+}
---
+++ Subroutine/Class1.cs
@@ -0,0 +1,21 @@
+﻿using SharpCompress.Common;
+using System;
+
+namespace Subroutine
+{
+    public class Class1
+    {
+        public void Run()
+        {
+            try
+            {
+                var archiveEncoding = new ArchiveEncoding();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
